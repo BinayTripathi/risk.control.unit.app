d4cc4754dd6694651e629ccadeb825e7054c3ee8
Binay <binay.mckv@gmail.com>
2023-07-01 10:08:53 +1000

    Updated package.json

--- src/util/getMapPreview.js
+++ src/util/getMapPreview.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'xyzzaSyC4WuLxLEUq5wr5rpPDPyEG99CRrrgzivfY';
+const GOOGLE_API_KEY = 'AIzaSyC4WuLxLEUq5wr5rpPDPyEG9CRrrgzivfY';
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
